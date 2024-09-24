ba9348e28bda971dcf064bee94bd9d3b1d331a92
L1b3rtar10 <166289032+L1b3rtar10@users.noreply.github.com>
2024-09-24 22:55:11 +0200

    Main program - change options and create lightning key

--- src/doomsdaywallet.cpp
+++ src/doomsdaywallet.cpp
@@ -3,12 +3,14 @@
 
 #include "features/seed_generator.h"
 #include "features/descriptors.h"
+#include "bitcoin/wallets.h"
+#include "bitcoin/jsonparser.h"
 
 using namespace std;
 
 // 64 random bytes
 #define RANDOM_SEED { \
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, \
+    0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, \
     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, \
     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, \
     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, \
@@ -18,7 +20,7 @@ using namespace std;
     0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00  \
 }
 
-#define EXIT_CODE '4'
+#define EXIT_CODE '6'
 
 #define OPTIONS_MENU_NO_SEED "\n\n \
     +++ Choose an option +++\n\
@@ -41,8 +43,10 @@ void createMenu(bool seedInitialised, optional<DescriptorMgr> descriptorMgr) {
     } else {
         cout << "-- 2. Generate account keys" << endl;
     }
-    cout << "-- 3. Export public key descriptors\n";
-    cout << "-- 4. Exit\n";
+    cout << "-- 3. List descriptors\n";
+    cout << "-- 4. Export Lightning node private key\n";
+    cout << "-- 5. Export descriptors to Bitcoin full node\n";
+    cout << "-- 6. Exit\n";
     cout << "Please select an option: ";
 }
 
@@ -68,38 +72,100 @@ int main(int argc, char **argv) {
     
     optional<SeedGenerator> seedGenerator = SeedGenerator::Make(argv[1]);
     optional<DescriptorMgr> descriptorMgr = nullopt;
+    optional<WalletMgr> wallet = nullopt;
+    optional<WalletMgr> lightningWallet = nullopt;
 
-    printMenu();
+    uint8_t testSeed[ENTROPY_SIZE] = {'\0'};
+    wallet = WalletMgr::Make("test_watchonly", testSeed);
+
+    createMenu(wallet.has_value(), descriptorMgr);
     
-    char choice;
-    scanf(" %c", &choice);
+    char choice = '\0';
 
     while (choice != EXIT_CODE) {
+        createMenu(wallet.has_value(), descriptorMgr);
+        choice = getchar();
         switch (choice) {
             case '1':
                 if (seedGenerator.has_value()) {
-                    seedGenerator->start(randomSeed);
-                    descriptorMgr = DescriptorMgr(seedGenerator->getSeed(), ENTROPY_SIZE);
-                    getchar();
+                    uint8_t masterSeed[ENTROPY_SIZE] = {'\0'};
+                    uint8_t lightningMasterSeed[ENTROPY_SIZE] = {'\0'};
+                    seedGenerator->start(randomSeed, masterSeed, lightningMasterSeed);
+                    cout << "\n\nSeed generated, press return to continue..." << endl;
+                    wallet = WalletMgr::Make("doomsday_wallet", masterSeed);
+                    lightningWallet = WalletMgr::Make("lightning_wallet", lightningMasterSeed);
+                    descriptorMgr = DescriptorMgr(masterSeed, ENTROPY_SIZE);
                 }
                 break;
             case '2':
-                if (descriptorMgr.has_value()) {
-                    descriptorMgr->start();
+                if (wallet.has_value()) {
+                    int accountNumber = 0;
+                    cout << "\n\nInsert an account number" << endl;
+                    cin >> accountNumber;
+                    wallet->generateAccountDescriptors(accountNumber);
+                    char c = getchar();
+                    cout << "\n\nAccount keys generated." << endl;
                 } else {
                     cout << "Master key must be initialised first.";
                 }
                 break;
             case '3':
-                if (descriptorMgr.has_value()) {
-                    descriptorMgr->exportDescriptors();
+                if (wallet.has_value()) {
+                    vector<string> descriptors = wallet->getDescriptors();
+                    vector<string>::iterator it;
+                    for (it = descriptors.begin(); it != descriptors.end(); it++) {
+                        cout << *it << endl;
+                    }
+                    char c = getchar();
+                    cout << "\n\nNow copy the descriptors if you need to." << endl;
+                    
+                } else {
+                    cout << "Wallet must be initialised first." << endl;
+                }
+                break;
+            case '4':
+                if (wallet.has_value()) {
+                    Key masterLightningKey = lightningWallet->exportMasterPrivKey();
+                    Key masterKey = wallet->exportMasterPrivKey();
+
+                    cout << "Lightning private key > " << masterLightningKey.exportXprivKey() << endl;
+                    char c = getchar();
+                    cout << endl;
+                    cout << "This is the key to create a lightning node hot wallet." << endl;
+                    cout << "Keep this key safe." << endl;
+                    
+                } else {
+                    cout << "Wallet must be initialised first." << endl;
+                }
+                break;
+            case '5':
+            /*
+                if (wallet.has_value()) {
+                    wallet->getBlockchainInfo();
+                    wallet->createWallet("xxxxxxxx", true);
+                    vector<string> descriptors = wallet->getDescriptors();
+                    vector<string>::iterator descriptor;
+                    for (descriptor = descriptors.begin(); descriptor != descriptors.end(); descriptor++) {
+                        optional<JsonObject> descriptorInfo = wallet->getDescriptorInfo(*descriptor);
+                        if(descriptorInfo.has_value()) {
+                            string descriptorImport = descriptorInfo->getChildAsString("descriptor");
+                            string response = wallet->importDescriptor(descriptorImport, "test_import");
+                        }
+                    }
+                    
                 } else {
                     cout << "Master key must be initialised first.";
                 }
+            */
                 break;
+            
         }
-        createMenu(seedGenerator->seedIsInitialised(), descriptorMgr);
-        scanf(" %c", &choice);
+        cout << "\nPress return to continue... " << endl;
+        char c = getchar();
+        while (c != '\n') {
+            cout << "Press return to continue... " << endl;
+            c = getchar();
+        }
     }
 
     return 0;
